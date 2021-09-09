942c6797911b7ee2069ae38ff3d5446594fba2ca
ktodatwo <190pa0ma99!>
2021-09-09 16:39:29 -0500

    Kat 090921 - added username to dashboardController and dashboard.html

--- src/main/java/com/example/BillTracker/controllers/DashboardController.java
+++ src/main/java/com/example/BillTracker/controllers/DashboardController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
@@ -21,27 +23,52 @@ public class DashboardController {
     @Autowired
     private UserRepository userRepository;
 
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        Optional<User> user = userRepository.findById(userId);
+        return user.get();
+    }
+
     @GetMapping("dashboard")
     //public String dashboard(Model model) {
-    public String dashboard(@RequestParam(required = false) Integer id, Model model) {
+    //public String dashboard(@RequestParam(required = false) Integer id, HttpServletRequest request, User user, Model model) {
+    public String dashboard(HttpServletRequest request, User user, Model model) {
+        //for graphic
         Gson gson = new Gson();
 
-        if (id == null) {
-            model.addAttribute("bill", billRepository.findAll());
-        } else {
-            Optional<User> bills = userRepository.findById(id);
-            if (bills.isEmpty()) {
-                model.addAttribute("title", "No bills yet");
-            } else {
-                User userBills = bills.get();
-                model.addAttribute("bill", userBills.getBills());
-            }
-        }
+        HttpSession session = request.getSession();
+        User theUser = getUserFromSession(session);
 
+        //if (id == null) {
+          //  model.addAttribute("bill", billRepository.findAll());
+        //} else {
+//            Optional<User> bills = userRepository.findById(id);
+//            if (bills.isEmpty()) {
+//                model.addAttribute("title", "No bills yet");
+//            } else {
+                //User userBills = bills.get();
+//                model.addAttribute("user", theUser.getFirstName());
+//                model.addAttribute("bill", theUser.getBills());
+
+         //model.addAttribute("bill", billRepository.findAll());
+         model.addAttribute("bill", theUser.getBills());
+         model.addAttribute("username", theUser.getFirstName());
+
+          //  }
+        //}
+
+
+        //related to graphic
         String billDataJson = gson.toJson(billRepository.findAll());
         model.addAttribute("billDataJson", billDataJson);
+
+
        // model.addAttribute("bill", billRepository.findAll());
         return "dashboard"; }
+
+
 
     @GetMapping("create-bill")
     public String createBill() {
